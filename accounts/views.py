b787849e4e9efdf5adb2b7e1b642f026267703f7
Joseph Sungpil Choi <chousemath@gmail.com>
2017-10-02 08:49:04 +0900

    some more checks

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.shortcuts import render
 
@@ -6,15 +7,24 @@ from django.shortcuts import render
 def signup(request):
     if request.method == 'POST':
         if request.POST['password'] == request.POST['password_confirmation']:
-            User.objects.create_user(
-                username=request.POST['username'],
-                email=request.POST['email'],
-                password=request.POST['password']
-            )
-            return render(request, 'accounts/signup.html', {
-                'ok': True,
-                'message': 'Welcome to the website!'
-            })
+            try:
+              user = User.objects.get(username=request.POST['username'])
+              # user = User.objects.get(email=request.POST['email'])
+              return render(request, 'accounts/signup.html', {
+                  'ok': False,
+                  'message': 'Username already taken.'
+              })
+            except User.DoesNotExist:
+                user = User.objects.create_user(
+                    username=request.POST['username'],
+                    email=request.POST['email'],
+                    password=request.POST['password']
+                )
+                login(request, user)
+                return render(request, 'accounts/signup.html', {
+                    'ok': True,
+                    'message': 'Welcome to the website!'
+                })
         else:
             return render(request, 'accounts/signup.html', {
                 'ok': False,
